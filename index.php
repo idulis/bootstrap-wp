8d0d286fac1b13ff2ea392f32f8ddb1ea20a6cb7
Disraelligua <disraelligua@gmail.com>
2015-02-23 00:08:14 +0100

    Añadimos los get_ de cabecera y pie de página
    Añadiendo los get_ debemos de crear los ficheros de "header.php" y "footer.php"

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
- 
+
+get_header(); //Corresponde a la creación del fichero "header.php"
  if(have_posts()) :
+   //Recorrido de todos los post que se vayan añadiendo
    white (have_posts()) : the_post() ;
     // Muestra el título con su enlace.
     <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
    
    endwhile;
 
-  else :
-    echo '<p>No hay contenido que mostrar.</p>';
-
+ else :
+   echo '<p>No hay contenido que mostrar.</p>';
+ 
+ endif;
+ 
+get_footer(); //Corresponde a la creación del fichero "footer.php"
 ?>
