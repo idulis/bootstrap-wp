033a9a6b5c69a22f6df23b15a29d88aa60655295
Disraelligua <disraelligua@gmail.com>
2015-02-22 19:50:53 +0100

    Loop 'have_posts()'
    Se inicia el loop diciendo <?php if (have_posts()) : while (have_posts()) : the_post(); ?>, si hay post en la base de datos haz lo siguiente

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+ 
+ if(have_posts()) :
+   white (have_posts()) : the_post() ;
+    // Muestra el título con su enlace.
+    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+   
+   endwhile;
 
-
+  else :
+    echo '<p>No hay contenido que mostrar.</p>';
 
 ?>
