3b6fa9bc8056982478b07a9eca0d08e18c7f5c17
Disraelligua <disraelligua@gmail.com>
2015-02-23 01:09:29 +0100

    Añadiendo el home_url y descripcion del sitio
    Con estas modificaciones conseguimos activar el título del sitio y la descripción del sitio.

--- header.php
+++ header.php
@@ -34,4 +34,34 @@
 -->
 <body <?php body_class(); ?> >
 
+<div class="container">
+
+	<header class="site-header">
+		<!-- home_url: recupera la URL principal del sitio actual. -->
+		<h1><a href="<?php echo home_url('/');?>">
+		<!-- Ya comentada -->
+		<?php bloginfo('name'); ?></a></h1>
+		<h5>
+			<?php bloginfo('description'); ?><!-- Muestra el "Lema", ambientada en Ajustes > Generales . Estos datos se recuperan del disco "BlogDescription" en la tabla wp_options. -->
+			<!-- 		
+				is_page ()
+				Cuando cualquier página se está mostrando.
+				is_page (42)
+				Cuando Página 42 (ID) se está visualizando.
+				is_page ("Sobre Mi Y Joe ')
+				Cuando la página se visualiza con un POST_TITLE de "Acerca de mí Y Joe".
+				is_page ('sobre-me')
+				Cuando la página se visualiza con un post_name (babosa) de "sobre-me".
+				is_page (array (42, 'sobre-yo "," Sobre Mi Y Joe'))
+				Devuelve cierto cuando las páginas mostradas es bien puesto ID = 42, o post_name es "acerca de mí", o POST_TITLE es "acerca de mí y Joe".
+				is_page (array (42, 54, 6))
+				Devuelve cierto cuando las páginas mostradas es bien puesto ID = 42, o enviados ID = 54, o enviados ID = 6.
+			-->
+			<!-- Visualiza la página cuyo post_name es "contenidos" -->
+			
+			<?php if(is_page('contenidos')) {?>
+				Gracias por visualizar este trabajo.
+			<?php }?>
+		</h5>
+	</header>
   
