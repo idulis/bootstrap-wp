6cd74d8f4e4596b6ab7e2c78e92e8104a3c6cec4
Disraelligua <disraelligua@gmail.com>
2015-02-24 03:16:27 +0100

    Actualizando, añadiendo args a nav
    Sino se pone nada entre las comillas simples ' ' ,significa que esta deshabilitado.
    
    <? Php
    
     $args = array (
            'Theme_location' => '',
    	 'Menú' => '',
    	 'Contenedor' => 'div',
    	 'Container_class' => '',
    	 'Container_id' => '',
    	 => 'Menú' 'menu_class',
    	 'Menu_id' => '',
    	 'Eco' => true,
    	 'Fallback_cb' => 'wp_page_menu',
    	 'Antes' => '',
    	 'Después' => '',
    	 'Link_before' => '',
    	 'Link_after' => '',
    	 'items_wrap' => '<ul id = "% 1 $ s" class = "% 2 $ s">% 3 $ s </ ul>',
    	 'Profundidad' => 0,
    	 'Walker' => ''       
     );
    
     wp_nav_menu ($args);
    
     ?>

--- header.php
+++ header.php
@@ -66,8 +66,16 @@
 		
 		
 		<nav class="site-nav">
+		
+			<?php
+			$args = array(
+				//Debemos de ir a functions.php para registrar este menú llamado 'primary'
+				'theme_location' => 'primary'
+			);
+			?>
+		
 			<!-- wp_nav_menu(): nos visualiza las páginas que hemos creado en wordPress: Pages -> Add New -->
-			<?php wp_nav_menu(); ?>
+			<?php wp_nav_menu($args); ?>
 		</nav>
 		
 		
