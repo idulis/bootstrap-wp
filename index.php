aabc6b4b83ea2a84338ba1653fd7bb0b8e51d549
Disraelligua <disraelligua@gmail.com>
2015-02-24 02:10:02 +0100

    Actuliazamos, añadiendo algunos estilos

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ get_header(); //Corresponde a la creación del fichero "header.php"
  if(have_posts()) :
    //Recorrido de todos los post que se vayan añadiendo
    white (have_posts()) : the_post() ;
-    // Muestra el título con su enlace.
-    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+   
+   <article class="post">
+     // Muestra el título con su enlace.
+     <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+   </article>
    
    endwhile;
 
