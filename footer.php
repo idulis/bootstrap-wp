83e80556c62df48361b47725fdaf8eaa5fbec5b7
Disraelligua <disraelligua@gmail.com>
2015-02-24 03:17:33 +0100

    Actualizando, añadiendo args a nav

--- footer.php
+++ footer.php
@@ -1,8 +1,15 @@
 <footer class="site-footer">
 
   <nav class="site-nav">
+  			<?php
+			$args = array(
+				//Debemos de ir a functions.php para registrar este menú llamado 'footer'
+				'theme_location' => 'footer'
+			);
+			?>
+			
 			<!-- wp_nav_menu(): nos visualiza las páginas que hemos creado en wordPress: Pages -> Add New -->
-			<?php wp_nav_menu(); ?>
+			<?php wp_nav_menu($args); ?>
 	</nav>
 
   <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
