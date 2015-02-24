ea6d64c3a1a85c9c23c00a0c9d6b5d8ada2f04cb
Disraelligua <disraelligua@gmail.com>
2015-02-24 02:40:07 +0100

    Actualizando, añadiendo menu

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
 <footer class="site-footer">
-<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
+
+  <nav class="site-nav">
+			<!-- wp_nav_menu(): nos visualiza las páginas que hemos creado en wordPress: Pages -> Add New -->
+			<?php wp_nav_menu(); ?>
+	</nav>
+
+  <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
 </footer>
 
 </div><!-- /Fin container-->
