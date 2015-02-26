1727fbb6dd9b5af1f310bdf19c2c6a7f71a83088
Disraelligua <disraelligua@gmail.com>
2015-02-26 01:21:44 +0100

    Registrando el menu: primary y footer
    WordPress 3.0 agregó menús de navegación personalizada que los usuarios pueden editar fácilmente en el panel de control. Antes de poder usar estos menús, necesitas un tema compatible con ellos. Agregas compatibilidad en el archivo "functions.php" (funciones) de un tema y luego añades las funciones necesarias para que aparezcan los menús en los archivos de plantillas. Usando la función register_nav_menus(), puedes agregar múltiples menús para editar en el panel de control, que es la programación técnica de WordPress. Una vez que acabes, puedes ingresar al panel de control y editar tus menús, agregando, borrando y reordenando enlaces sin ninguna programación.
    
    Instrucciones
    
    1
    Ingresa a tu panel de control de WordPress y navega a "Apariencia". Haz clic en "Editor" debajo de "Apariencia" y presiona el enlace debajo de "Funciones de tema" en el lado derecho del cuadro de edición del tema. Si trabajas con los archivos de tema en tu computadora, puedes abrir "functions.php" en tu editor de códigos en su lugar. Ese archivo se encuentra aquí: /yoursite/wp-content/themes/theme-folder/
    
    2
    Agrega este código en algún lugar de tu archivo "functions.php": if (function_exists('register_nav_menus')) { register_nav_menus( array( 'first_menu_name' => 'My First Menu', 'second_menu_name' => 'My Second Menu' ) ); } Esta función revisa si la función register_nav_menus() existe en tu versión actual de WordPress y luego l a usa para registrar una gran cantidad de menús. Puedes agregar uno, dos o cualquier otro número de menús que quieras que usen esta función.

--- functions.php
+++ functions.php
@@ -12,4 +12,15 @@ function learningWordPress_resources(){
 // wp_enqueue_scripts: Una forma segura de añadir JavaScripts a una página generada por WordPress.
 add_action('wp_enqueue_scripts', 'learningWordPress_resources');
 
+
+/* ============================================
+*	Navigation Menu
+*	register_nav_menu()
+*	Esta función revisa si la función register_nav_menus() existe en tu versión actual de WordPress y luego l a usa para registrar una gran cantidad de menús. Puedes agregar uno, dos o cualquier otro número de menús que quieras que usen esta función.
+============================================*/
+register_nav_menu(array(
+	'primary' => __('Primary Menu'),
+	'footer' => __('Footer Menu')
+));
+
 ?>
