a1c47977688271990d3f8bc05096264970a5bbaa
Disraelligua <disraelligua@gmail.com>
2015-02-26 02:41:57 +0100

    Correcciones en el código

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
 get_header(); //Corresponde a la creación del fichero "header.php"
  if(have_posts()) :
    //Recorrido de todos los post que se vayan añadiendo
-   white (have_posts()) : the_post() ;
+   white (have_posts()) : the_post() ; ?>
    
    <article class="post">
      // Muestra el título con su enlace.
      <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+     //Muestra el contenido de cada una de las páginas
+     <?php the_content(); ?>
    </article>
    
-   endwhile;
+   <?php endwhile;
 
  else :
    echo '<p>No hay contenido que mostrar.</p>';
