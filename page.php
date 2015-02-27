edf48d291bf3aeebff430503425804d99564cc50
Disraelligua <disraelligua@gmail.com>
2015-02-27 11:08:11 +0100

    Mostrando título, enlace y menu child_of
    Mostramos el enlace y titulo de la 'página padre': Línea 13
    
    Luego creamos un menú que va ha contener todas las 'páginas hijas', <nav class="site-nav children-links clearfix">: Línea 9
    
    Damos algo de estilos /* Children Links */

--- page.php
+++ page.php
@@ -5,16 +5,33 @@ get_header(); //Corresponde a la creación del fichero "header.php"
    white (have_posts()) : the_post() ; ?>
    
    <article class="post page">
-    <?php 
-     $args = array(
-      'child_of' => get_top_ancestor_id(),
-      'title_li' => ''
-     );
-    ?>
    
-    <?php wp_list_pages($args); ?>
-   
-   
+     <nav class="site-nav children-links clearfix">
+     			<span class="parent-link">
+     			<!-- get_the_permalink($ID), Devuelve el enlace permanente a un post o una página para su uso.-->
+     			<!-- get_the_title($ID), Esta función devolverá el título de un post para un determinado post-ID -->
+     			<a href="<?php echo get_the_permalink(get_top_ancestor_id()); ?>">
+     			<?php echo get_the_title(get_top_ancestor_id()); ?></a>
+     
+     			</span>
+     			<ul>
+     				<?php
+     
+     				/*--  --*/
+     				$args = array(
+     					/*-- child_of, Lista las sub-páginas de una sola página --*/
+     					/*-- utiliza el ID de una página como valor --*/
+     					/*-- 'child_of' => $post->ID, El valor predeterminado es 0 (Lista todas las sub-páginas) --*/
+     					/*-- --*/
+     					'child_of' => get_top_ancestor_id(),
+     					'title_li' => ''
+     				);
+     
+     				/*-- Esta función devuelve un array de páginas que están en el blog --*/
+     				/*-- Recibe un argumento $args --*/
+     				wp_list_pages($args); ?>
+     			</ul>
+     		</nav><!-- /Fin de sub-menu -->
    
      // Muestra el título con su enlace.
      <h2><?php the_title(); ?></h2>
